36714a1df7f6da2398dcedbd443ff9422464d535
Maria Clara Guerra <maria.calvarenga@eaportal.org>
2025-10-15 09:30:25 -0300

    Add Google OAuth authorization params

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -8,6 +8,13 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code"
+        }
+      }
     }),
     FacebookProvider({
       clientId: process.env.FACEBOOK_CLIENT_ID || '',
